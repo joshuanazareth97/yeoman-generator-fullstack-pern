0444cafb4ef7ee933bdc7c109ed0621acb5f4981
Joshua <joshuanazareth97@gmail.com>
2022-01-03 23:36:16 +0530

    Write script to install subdependencies

--- generators/app/templates/base/install_deps.sh
+++ generators/app/templates/base/install_deps.sh
@@ -1,3 +1,9 @@
-for filename in ./**; do
-    echo $filename
+#! bin/sh
+
+for directory in ./*/; do
+    echo "Checking $directoryi ..."
+    chmod o+x "$directory"
+    cd "$directory"
+    test -f "./package.json" && echo "Installing deps..." && npm install
+    cd .. && echo "\n"
 done
